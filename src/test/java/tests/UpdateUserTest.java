93e626c07c22f4728f71c2aeec5e5c6bec57cbb5
Your Name <you@example.com>
2026-07-21 15:51:31 +0300

    Refactor UpdateUserTest

--- src/test/java/tests/UpdateUserTest.java
+++ src/test/java/tests/UpdateUserTest.java
@@ -1,30 +1,37 @@
 package tests;
 
-import io.restassured.RestAssured;
+import api.UserApi;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.restassured.response.Response;
 import models.User;
 import org.junit.jupiter.api.Test;
-import utils.ConfigReader;
+import utils.ResponseValidator;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static utils.ResponseSpecs.success200;
 
 public class UpdateUserTest {
 
     @Test
+    @Owner("Marius")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify that an existing user can be updated successfully.")
     public void updateUserTest() {
 
-        RestAssured.baseURI = ConfigReader.get("baseUrl");
+        UserApi userApi = new UserApi();
 
-        User user = new User("Marius", "Senior QA");
+        User user = new User(
+                "Marius",
+                "Senior QA"
+        );
 
-        given()
-                .contentType("application/json")
-                .body(user)
-                .when()
-                .put("/api/users/2")
-                .then()
-                .statusCode(200)
-                .body("name", equalTo("Marius"))
-                .body("job", equalTo("Senior QA"));
+        Response response = userApi.updateUser(2, user);
+
+        response.then().spec(success200());
+
+        ResponseValidator.validateField(response, "name", "Marius");
+        ResponseValidator.validateField(response, "job", "Senior QA");
     }
 }
